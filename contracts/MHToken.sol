82d8e379e8ff440ba01b8d9952a09fa1389e23e0
Teddy <inteldev2080@gmail.com>
2021-10-20 09:09:36 +0300

    feat: add ERC20 token

---
+++ contracts/MHToken.sol
@@ -0,0 +1,11 @@
+// contracts/NFT.sol
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MHToken is ERC20 {
+    constructor() ERC20("MHToken", "MHT") {
+        _mint(msg.sender, 100000 * (10 ** 18));
+    }
+}
