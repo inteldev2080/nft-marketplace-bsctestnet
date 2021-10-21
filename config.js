94afd5f477033a374ec6510e510d3dd823657b5e
Teddy <inteldev2080@gmail.com>
2021-10-21 14:09:13 +0300

    deploy nft contracts to bsctestnet

---
+++ config.js
@@ -0,0 +1,5 @@
+
+  export const nftmarketaddress = "0xE0950e809Ae98f50fBf0Ff784Eccfab3e300970d"
+  export const nftaddress = "0x5Fd3584Ce330eAf7066281094E466E74040114E2"
+  export const rpc_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
+  
