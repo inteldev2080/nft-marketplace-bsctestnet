252ff9ab6c2a701e0a34e71ff230115832e6d65e
Teddy <inteldev2080@gmail.com>
2021-10-21 13:18:24 +0300

    fix: add rpc_url to config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
   export const nftmarketaddress = "0x1b8F05a554Ec4F5E5Fe3134E653bC3720ed46941"
   export const nftaddress = "0xB5E01151B6523A6448516BbD36eC88A99753705c"
-  
+  export const rpc_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
