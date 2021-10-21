46c08fec6578aa00f1d5ff9b6167ca4086995f0c
Teddy <inteldev2080@gmail.com>
2021-10-21 09:57:36 +0300

    fix: a bit change in heroku-postbuild

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
-// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -12,7 +12,7 @@ module.exports = {
     /*
     mumbai: {
       // Infura
-      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
       url: "https://rpc-mumbai.matic.today",
       accounts: [privateKey]
     },
@@ -23,6 +23,11 @@ module.exports = {
       accounts: [privateKey]
     }
     */
+
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${infuraId}`,
+      accounts: [privateKey]
+    }
   },
   solidity: {
     version: "0.8.4",
