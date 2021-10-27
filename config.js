3324ca2cbf92dcf93c1ef65deb2d58f1782d7afa
Teddy <inteldev2080@gmail.com>
2021-10-27 12:23:00 +0900

    test: redeploy nft to bsctestnet

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
-  export const nftmarketaddress = "0x99D5484fa2DBe938aCa8F4F9463a4de5A1096D21"
-  export const nftaddress = "0xD5285D11BD83EE7B1BfFc92ea60F556299C7D033"
+  export const nftmarketaddress = "0xB1487D59f65dFEae8f32fF3d3B36BBfEDCBE9598"
+  export const nftaddress = "0xb14E0477E2Bb495df53EE11b01F74D9D3806Da3e"
   export const rpc_url = "https://data-seed-prebsc-1-s1.binance.org:8545"
   
